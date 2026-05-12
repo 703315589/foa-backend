e39b05f9784902a43574e88fa47f30e79f889b06
703315589 <mohammad.farhan@genpact.com>
2026-05-13 00:38:56 +0530

    test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const generateQr = require('./routes/generate-qr');
 app.use('/api/payment-link', paymentRoutes);
 app.use('/api/generate-qr', generateQr);
 app.get("/", (req, res) => {
-  res.send("Deployed version v2 🚀 on branch dev flkjdklfjd");
+  res.send("Deployed version v2 🚀 on branch dev test");
 });
 
 
