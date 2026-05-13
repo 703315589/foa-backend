40534dc8b5c99a2d83ca68491ba8c5420eb9ac93
703315589 <mohammad.farhan@genpact.com>
2026-05-13 16:27:35 +0530

    port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
